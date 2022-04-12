0e0bc795e7d834f20523dc11b2e09b16c85ca1e7
square <artubr@gmail.com>
2022-04-12 23:59:15 +0200

    Header component init

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="header"><ul><li></li></ul>Dota 2 Leaderboard </div>
+  )
+}
--- pages/hello.tsx
+++ pages/hello.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
+import Header from '../components/Header'
 
 const fetcher = async (url) => {
   const res = await fetch(url)
@@ -20,7 +21,8 @@ export default function Person() {
   if (error) return <div>{error.message}</div>
   if (!data) return <div>Loading...</div>
 
-  return (
-      <h1>Digamos {data.name}</h1>
+    return (
+      <><Header /><h1>Digamos {data.name}</h1></>
+      
   )
 }
